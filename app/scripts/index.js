45422bc7b6878beb4cc64b06878882642353124b
Pavel <yashchenkodev@gmail.com>
2025-08-12 03:30:20 +0300

    finished

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,3 +1,23 @@
+const initMobileMenu = () => {
+  const mobileMenuButton = document.querySelector('.header__mobile-button');
+  const mobileMenuInner = document.querySelector('.header__mobile .header__menu');
+  const menuLinks = document.querySelectorAll('.header__mobile .header__menu-item');
+
+  const toggleMenu = () => {
+    mobileMenuInner.classList.toggle('is-active');
+    document.body.classList.toggle('scroll-block');
+  };
+
+  menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      mobileMenuInner.classList.remove('is-active');
+      document.body.classList.remove('scroll-block');
+    });
+  });
+
+  mobileMenuButton.addEventListener('click', toggleMenu);
+};
+
 const initAccordion = () => {
   new Accordion('.page-section__column--accordion', {
     duration: 400,
@@ -7,5 +27,6 @@ const initAccordion = () => {
 
 document.addEventListener('DOMContentLoaded', function () {
   new WOW().init();
+  initMobileMenu();
   initAccordion();
 });
